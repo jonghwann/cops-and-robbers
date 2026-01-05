fea1a5db0c346f950e4be952157684aff490c6ad
jonghwann <jonghwan0729@gmail.com>
2026-01-05 15:52:02 +0900

    feat: supabase 연동 완료

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,43 @@
-import { Text, View } from 'react-native';
+import { useEffect, useState } from 'react';
+import { ActivityIndicator, Text, View } from 'react-native';
+import { supabase } from '@/utils/supabase';
+import './global.css';
 
 export default function Index() {
+  const [status, setStatus] = useState<'loading' | 'success' | 'error'>('loading');
+
+  useEffect(() => {
+    const testConnection = async () => {
+      try {
+        const { error } = await supabase.auth.getSession();
+
+        if (error) {
+          console.error(error);
+          setStatus('error');
+        } else {
+          setStatus('success');
+        }
+      } catch (error) {
+        console.error(error);
+        setStatus('error');
+      }
+    };
+
+    testConnection();
+  }, []);
+
   return (
-    <View>
-      <Text className="text-wrap rounded-md bg-red-500 p-2 px-2 font-bold text-red-300">
-        Hello World !! NativeWind
-      </Text>
+    <View className="flex-1 items-center justify-center bg-white">
+      <Text className="mb-4 font-bold text-2xl">👮🏻‍♂️ Cops and Robbers 💰</Text>
+
+      {status === 'loading' && (
+        <>
+          <ActivityIndicator size="large" color="#4F46E5" />
+          <Text className="mt-2 text-gray-600">Connecting to Supabase...</Text>
+        </>
+      )}
+      {status === 'success' && <Text className="text-green-600">✅ Supabase Connected!</Text>}
+      {status === 'error' && <Text className="text-red-600">❌ Connection Failed</Text>}
     </View>
   );
 }
