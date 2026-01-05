3badd82d39c52d2f7249aec82b67e2c15c34ad89
jonghwann <jonghwan0729@gmail.com>
2026-01-05 21:18:43 +0900

    feat: 글로벌 색상 추가

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
-  presets: [require("nativewind/preset")],
+  content: ['./app/**/*.{js,jsx,ts,tsx}', './components/**/*.{js,jsx,ts,tsx}'],
+  presets: [require('nativewind/preset')],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#48a0f8',
+        secondary: '#e5e7eb',
+        tertiary: '#909090',
+      },
+    },
   },
   plugins: [],
-}
-
+};
